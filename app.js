74619bc7583d766b553bf87406f6ac89881ff995
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-10-17 12:59:27 +0700

    todo.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const todoRoutes = require('./routes/todo.js');
+const port = 4000;
 
+app.use(express.json());
+app.use('/todos', todoRoutes);
 app.set('view engine','ejs');
 
 app.get('/', (req, res) => {
